3d6570c298abf51e46cf74efacee8bf4c0f24f7a
soumenmt <soumenmt@gmail.com>
2023-01-19 11:24:04 -0700

    git config --global user.name

--- Examples/js/earthquakeUSGS.js
+++ Examples/js/earthquakeUSGS.js
@@ -8,21 +8,17 @@
             id: "id",
             dataType: tableau.dataTypeEnum.string
         }, {
-            id: "mag",
-            alias: "magnitude",
-            dataType: tableau.dataTypeEnum.float
-        }, {
             id: "title",
             alias: "title",
             dataType: tableau.dataTypeEnum.string
         }, {
             id: "location",
-            dataType: tableau.dataTypeEnum.geometry
+            dataType: tableau.dataTypeEnum.string
         }];
 
         var tableSchema = {
-            id: "earthquakeFeed",
-            alias: "Earthquakes with magnitude greater than 4.5 in the last seven days",
+            id: "cartaddressfeed",
+            alias: "CART Address Feed",
             columns: cols
         };
 
@@ -31,17 +27,16 @@
 
     // Download the data
     myConnector.getData = function(table, doneCallback) {
-        $.getJSON("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/4.5_week.geojson", function(resp) {
-            var feat = resp.features,
+        $.getJSON("https://bms-cart-dev1.pegacloud.net/prweb/api/CARTREMSAddress/v1/Address", function(resp) {
+            var feat = resp.pxResults,
                 tableData = [];
 
             // Iterate over the JSON object
             for (var i = 0, len = feat.length; i < len; i++) {
                 tableData.push({
-                    "id": feat[i].id,
-                    "mag": feat[i].properties.mag,
-                    "title": feat[i].properties.title,
-                    "location": feat[i].geometry
+                    "id": feat[i].address_id,
+                    "location": feat[i].nameOfCountry,
+                    "title": feat[i].nameOfCountry
                 });
             }
 
@@ -55,7 +50,7 @@
     // Create event listeners for when the user submits the form
     $(document).ready(function() {
         $("#submitButton").click(function() {
-            tableau.connectionName = "USGS Earthquake Feed"; // This will be the data source name in Tableau
+            tableau.connectionName = "CART Feed"; // This will be the data source name in Tableau
             tableau.submit(); // This sends the connector object to Tableau
         });
     });
